2759e6e3aafff70dd55c06017a3ca8a767ff8c1d
Jon Swenson <jmswen@gmail.com>
2024-12-12 13:15:50 -0800

    [thrift] setup.py to build lib/python

---
+++ thrift/lib/python/setup.py
@@ -0,0 +1,29 @@
+from Cython.Build import cythonize
+from Cython.Compiler import Options
+from setuptools import Extension, setup
+
+
+Options.fast_fail = True
+
+exts = [
+    Extension(
+        "thrift.python.protocol",
+        sources=["protocol.pyx"],
+        language="c++",
+        extra_compile_args=["-std=c++17"],
+    ),
+]
+
+setup(
+    name="thrift",
+    version="0.0.1",
+    packages=["thrift", "thrift.python"],
+    package_data={"": [".*pxd", "*.h"]},
+    setup_requires=["cython"],
+    zip_safe=False,
+    ext_modules=cythonize(
+        exts,
+        compiler_directives={"language_level": 3},
+    ),
+)
+

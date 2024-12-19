22b6ac6e3679ce46d70a448d54af68112d297cf8
Jon Swenson <jmswen@gmail.com>
2024-12-19 09:54:58 -0800

    More Python types support

--- thrift/lib/setup.py
+++ thrift/lib/setup.py
@@ -3,6 +3,8 @@
 # Cython requires source files in a specific structure, the structure is
 # created as tree of links to the real source files.
 
+import sys
+
 from Cython.Build import cythonize
 from Cython.Compiler import Options
 from setuptools import Extension, setup
@@ -13,95 +15,110 @@ exts = [
     Extension(
         "thrift.python.adapter",
         sources=["thrift/python/adapter.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.common",
         sources=["thrift/python/common.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.converter",
         sources=["thrift/python/converter.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.exceptions",
         sources=["thrift/python/exceptions.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.flags",
         sources=["thrift/python/flags.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.mutable_containers",
         sources=["thrift/python/mutable_containers.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.mutable_exceptions",
         sources=["thrift/python/mutable_exceptions.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.mutable_serializer",
         sources=["thrift/python/mutable_serializer.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.mutable_typeinfos",
         sources=["thrift/python/mutable_typeinfos.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.mutable_types",
         sources=["thrift/python/mutable_types.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.protocol",
         sources=["thrift/python/protocol.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
     Extension(
         "thrift.python.serializer",
         sources=["thrift/python/serializer.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
-        language="c++",
-        extra_compile_args=["-std=c++17"],
-    ),
-    Extension(
-        "thrift.python.types",
-        sources=["thrift/python/_types.pyx"],
-        libraries=["folly", "thriftcpp2", "thrift_python_serializer"],
+        libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
         language="c++",
         extra_compile_args=["-std=c++17"],
     ),
 ]
+
+if "build_ext" not in sys.argv:
+    exts.append(
+        Extension(
+            "thrift.python.types",
+            sources=["thrift/python/_types.pyx"],
+            libraries=["folly", "thriftcpp2"],
+            language="c++",
+            extra_compile_args=["-std=c++17"],
+        )
+    )
+elif "--types-only" in sys.argv:
+    exts = [
+        Extension(
+            "thrift.python.types",
+            sources=["thrift/python/_types.pyx"],
+            libraries=["folly", "thriftcpp2", "thrift_python_cpp"],
+            language="c++",
+            extra_compile_args=["-std=c++17"],
+        )
+    ]
+    sys.argv.remove("--types-only")
 
 setup(
     name="thrift",
